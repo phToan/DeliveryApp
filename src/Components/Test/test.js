155b971f61ff91d673bcda4255b746c51893981d
phToan <phamhaitoan10@gmail.com>
2024-02-26 09:35:59 +0700

    add text

---
+++ src/Components/Test/test.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+export const Test = () => {
+    return (
+        <View>
+            <Text>ok</Text>
+        </View>
+    )
+}
